7ff84dd35b3d76e15d3cbf57cb7d33af1471409e
nishant <nishant.singh87@gmail.com>
2020-10-10 20:38:15 +0530

    Enable highlighting and style.

--- scripts/build-markdown.js
+++ scripts/build-markdown.js
@@ -1,18 +1,18 @@
 const remarkable = require('remarkable');
+const hljs = require('highlight.js');
 const glob = require('glob');
 const fs = require('fs');
 const path = require('path');
 const markdownTemplate = require('../src/md-template');
-
 // Actual default values
-const markdown = new remarkable.Remarkable({
-  html:         false,        // Enable HTML tags in source
+const markdown = new remarkable.Remarkable('full',{
+  html:         true,        // Enable HTML tags in source
   xhtmlOut:     false,        // Use '/' to close single tags (<br />)
   breaks:       false,        // Convert '\n' in paragraphs into <br>
   langPrefix:   'language-',  // CSS language prefix for fenced blocks
 
   // Enable some language-neutral replacement + quotes beautification
-  typographer:  false,
+  typographer:  true,
 
   // Double + single quotes replacement pairs, when typographer enabled,
   // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
@@ -20,7 +20,23 @@ const markdown = new remarkable.Remarkable({
 
   // Highlighter function. Should return escaped HTML,
   // or '' if the source string is not changed
-  highlight: function (/*str, lang*/) { return ''; }
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (err) {
+      console.error(err);
+    }
+
+    return ''; // use external default escaping
+  }
 });
 
 const getAllMarkdowns = async function (src) {
